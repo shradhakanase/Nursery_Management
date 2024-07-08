7dde79f8dcd1f32707386e864ddb532d31d7a304
shradhakanase <shradhakanase1722002@gmail.com>
2024-07-08 12:02:43 +0530

    Preparing Backend

---
+++ NurseryFrontend/src/test/java/com/srk/NurseryFrontend/NurseryFrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.srk.NurseryFrontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NurseryFrontendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
